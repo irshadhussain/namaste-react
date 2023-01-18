4de3480bff94efa1ab0912ed52a16420f8a5145b
Irshad <irshad.hussain26@gmail.com>
2023-01-19 01:21:42 +0530

    Chapter 2 => ignite the app

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const heading1 = React.createElement("h1",{key:"heading1", id:"heading1", className:"heading1", style:{backgroundColor:"blue"}},"Heading 1");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1",{key:"heading1", id:"heading1", className:"heading1", style:{backgroundColor:"blue"}},"Heading 1 from parcel");
 const heading2 = React.createElement("h2",{key:"heading2", id:"heading2", className:"heading2",},"Heading 2");
 const container = React.createElement("div",{id:"container"},[heading1, heading2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
